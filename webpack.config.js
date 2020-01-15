df4640d836baca55771fbc09d46b0b8b00e96512
crisrauber <cristianaugusto1@hotmail.com>
2020-01-14 21:37:33 -0300

    new bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const  path = require('path');
-const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: ['@babel/polyfill',path.resolve(__dirname, 'src', 'index.js')],
@@ -10,12 +9,6 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
   },
-  plugins:[
-    new Dotenv({
-      path: path.resolve(__dirname, '.env')
-      // safe: false // load .env.example (defaults to "false" which does not use dotenv-safe)
-    }),
-  ],
   module: {
     rules: [
       {
